ae126b65f1990b9a0f9ddab56f1f33164c9f2056
Malik Elate <Malik.e1955@gmail.com>
2021-12-18 16:08:56 -0600

    able to get token from .env file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
+const axios = require('axios');
 
 const App = () => {
+  const token = process.env.REACT_APP_CANVAS_INFRASTRUCTURE_API_KEY;
+  console.log("token: ", token)
   return (
-    
+
     <div className="App">
-      {/*  
-        1. site header with name of product 
-        2. information: address, host, time of event, image of location
-        3. down the line: SEO optimizing pages: about, privacy, and contact information
-      */}
-        <ul>Site Header</ul>
+      <a href={"https://canvas.instructure.com/api/v1/courses?access_token=" + token}>GO here</a>
+      <button
+        style={{ marginTop: "100px" }}
+        onClick={() => {
+          console.log("Api button clicked");
+          const config = {
+            headers: { Authorization: `Bearer ${token}` }
+          };
+          const bodyParameters = {
+            key: "value"
+          };
+          axios.get(
+            `https://canvas.instructure.com/api/v1/courses?access_token=${token}`,
+            config,
+            bodyParameters
+          )
+            .then(function (res) {
+              console.log(res);
+            })
+            .catch(function (err) {
+              console.log("error in canvas api request", err);
+            }).then(function () {
+            })
+          // curl https://canvas.ubc.ca/api/v1/users/self -H “Authorization: Bearer $token”
+        }}
+      >Call to canvas</button>
+      {/* out indent Shift + Option + F. */}
+      <header className="k">Site Header</header>
     </div>
   );
 }
